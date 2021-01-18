1f3ff6ea0a5e2f624d38da027a155c15cf1d9a30
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2021-01-18 14:21:09 +0100

    Implement caesar encrypt (refactorized)

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
+
+//Refakrotisera = städa, omorganisera kod utan att ändra funtionalitet
+
+
+function caesarEncrypt(msg) {
+
+    // 1. översätta varje tecken till ett nummer
+    const charCodes = []
+    for (let i = 0; i < msg.length; i++) {
+        charCodes.push(msg[i].charCodeAt(0))
+
+    }
+
+    // 2. shifta det numret
+    for (let i = 0; i < charCodes.length; i++) {
+        charCodes[i] = charCodes[i] + 1
+    }
+
+    let result = ""
+    // 3. översätta  numret till tecken, charCode till string
+    for (let i = 0; i < charCodes.length; i++) {
+        result = result + String.fromCharCode(charCodes[i])
+    }
+    return result
+}
+
 const secretMessage = "ATTACKATSEVEN"
+const encryptedMessage = caesarEncrypt(secretMessage)
+
 console.log(secretMessage)
-
-// 1. översätta varje tecken till ett nummer
-const charCodes = []
-for (let i = 0; i < secretMessage.length; i++) {
-    charCodes.push(secretMessage[i].charCodeAt(0))    
-    
-}
-
-// 2. shifta det numret
-for (let i = 0; i < charCodes.length; i++) {
-    charCodes[i] = charCodes[i] + 1
-}
-
-let encryptedMessage = ""
-// 3. översätta  numret till tecken, charCode till string
-for (let i = 0; i < charCodes.length; i++) {
-    encryptedMessage = encryptedMessage + String.fromCharCode(charCodes[i])
-}
 console.log(encryptedMessage)
